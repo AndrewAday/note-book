aa7b8a31b8c94075326a82ed2004669320437faa
Andrew Aday <andrew.aday@columbia.edu>
2016-09-10 17:02:39 -0400

    first commit

---
+++ spec/note-book-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NoteBookView from '../lib/note-book-view';
+
+describe('NoteBookView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
